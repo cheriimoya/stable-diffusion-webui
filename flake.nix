80df83c7271c6d356f1ea09e95a5916b2b5fd05c
Max Hausch <github@hausch.xyz>
2024-09-26 22:33:14 +0200

    feat: Move custom packages to let block

--- flake.nix
+++ flake.nix
@@ -13,29 +13,53 @@
         overlays = [ (_: final: { cudaPackages = final.cudaPackages_12_3; }) ];
       };
 
+      blendmodes = pkgs.python3Packages.buildPythonPackage rec {
+        pname = "blendmodes";
+        version = "2024.1.1";
+        pyproject = true;
+
+        src = pkgs.fetchPypi {
+          inherit pname version;
+          hash = "sha256-pARiphBO19gklFVzrHVwcUKe5/o+3tJ71pfR06D11Hc=";
+        };
+        buildInputs = [ pkgs.python3Packages.poetry-core ];
+        checkInputs = with pkgs.python3Packages; [
+          pillow
+          aenum
+          numpy
+        ];
+      };
+
+      tomesd = pkgs.python3Packages.buildPythonPackage rec {
+        pname = "tomesd";
+        version = "v0.1.3";
+        src = pkgs.fetchFromGitHub {
+          repo = pname;
+          owner = "dbolya";
+          rev = version;
+          hash = "sha256-U3LN6KmQx/ulepFxjWgYHJl5g8j1U3HIGunpjcZBcos=";
+        };
+      };
+
+      pillow-avif-plugin = pkgs.python3Packages.buildPythonPackage rec {
+        pname = "pillow-avif-plugin";
+        version = "v1.4.6";
+        src = pkgs.fetchFromGitHub {
+          repo = pname;
+          owner = "fdintino";
+          rev = version;
+          hash = "sha256-fppFgGrtj5c6eR3B3vodM9sicSHcKdpL7tUPL9PDUD0=";
+        };
+        buildInputs = [ pkgs.libavif ];
+      };
+
       python_env = pkgs.python3.withPackages (
         ps: with ps; [
           GitPython
           pillow
           accelerate
 
-          # blendmodes
-          (pkgs.python3Packages.buildPythonPackage rec {
-            pname = "blendmodes";
-            version = "2024.1.1";
-            pyproject = true;
-
-            src = pkgs.fetchPypi {
-              inherit pname version;
-              hash = "sha256-pARiphBO19gklFVzrHVwcUKe5/o+3tJ71pfR06D11Hc=";
-            };
-            buildInputs = [ pkgs.python3Packages.poetry-core ];
-            checkInputs = with pkgs.python3Packages; [
-              pillow
-              aenum
-              numpy
-            ];
-          })
+          blendmodes
 
           clean-fid
           diskcache
@@ -61,36 +85,14 @@
           safetensors
           scikit-image
 
-          # tomesd
-          (pkgs.python3Packages.buildPythonPackage rec {
-            pname = "tomesd";
-            version = "v0.1.3";
-            src = pkgs.fetchFromGitHub {
-              repo = pname;
-              owner = "dbolya";
-              rev = version;
-              hash = "sha256-U3LN6KmQx/ulepFxjWgYHJl5g8j1U3HIGunpjcZBcos=";
-            };
-          })
+          tomesd
 
           torch
           torchdiffeq
           torchsde
           transformers
 
-          # pillow-avif-plugin
-          (pkgs.python3Packages.buildPythonPackage rec {
-            pname = "pillow-avif-plugin";
-            version = "v1.4.6";
-            src = pkgs.fetchFromGitHub {
-              repo = pname;
-              owner = "fdintino";
-              rev = version;
-              hash = "sha256-fppFgGrtj5c6eR3B3vodM9sicSHcKdpL7tUPL9PDUD0=";
-            };
-
-            buildInputs = [ pkgs.libavif ];
-          })
+          pillow-avif-plugin
 
           opencv4
           dctorch
@@ -105,6 +107,9 @@
     in
     {
       devShells.x86_64-linux.default = pkgs.mkShell { buildInputs = [ python_env ]; };
-      packages.x86_64-linux.opencv4 = pkgs.python3Packages.opencv4;
+      packages.x86_64-linux = {
+        inherit python_env;
+        inherit (pkgs.python3Packages) opencv4;
+      };
     };
 }
